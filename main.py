3001666c271c2fa763c42a9a139173e516fcbbd0
Taha <tahaelsenosy@gmail.com>
2019-06-18 10:54:20 +0200

    Set the main view to choose a method

--- main.py
+++ main.py
@@ -166,5 +166,29 @@ def newton():
     
 
 if __name__ == "__main__":
-    # bisection()
-    newton()
+    print("""                                                                                                               
+    
+8888888  .d8888b.   .d8888b.  8888888b.        888b    888                                         d8b                   888 
+  888   d88P  Y88b d88P  Y88b 888   Y88b       8888b   888                                         Y8P                   888 
+  888   Y88b.      Y88b.      888    888       88888b  888                                                               888 
+  888    "Y888b.    "Y888b.   888   d88P       888Y88b 888 888  888 88888b.d88b.   .d88b.  888d888 888  .d8888b  8888b.  888 
+  888       "Y88b.     "Y88b. 8888888P"        888 Y88b888 888  888 888 "888 "88b d8P  Y8b 888P"   888 d88P"        "88b 888 
+  888         "888       "888 888 T88b         888  Y88888 888  888 888  888  888 88888888 888     888 888      .d888888 888 
+  888   Y88b  d88P Y88b  d88P 888  T88b        888   Y8888 Y88b 888 888  888  888 Y8b.     888     888 Y88b.    888  888 888 
+8888888  "Y8888P"   "Y8888P"  888   T88b       888    Y888  "Y88888 888  888  888  "Y8888  888     888  "Y8888P "Y888888 888                                                                                           
+    """)
+
+    method = int(input("""
+    Please Select an method
+        1) Bisection
+        2) Newton 
+        
+        Choice: """))
+
+    if method == 1:
+        bisection()
+    elif method == 2:
+        newton()
+    else:
+        print("Unknown choice..")
+        quit()
